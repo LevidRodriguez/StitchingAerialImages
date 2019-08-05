a03406f0931be959a0f98b2205340a84cb5ba3e7
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-08-05 18:59:24 -0500

    Ok

--- stitch.py
+++ stitch.py
@@ -19,5 +19,4 @@ if __name__ == '__main__':
     if ( len(sys.argv) < 4 ):
         print ("Usage: %s <image_dir> <key_frame> <output>" % sys.argv[0])
         sys.exit(-1)
-    
     Stitch(sys.argv[1], sys.argv[2], sys.argv[3])

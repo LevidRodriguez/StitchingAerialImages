99bc6b8198ae2644c51151aeb1ebaed892ffeb2b
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-08-27 18:05:17 -0500

    Add images

--- StitchPair.py
+++ StitchPair.py
@@ -2,15 +2,12 @@ import cv2
 import numpy as np
 
 def stitchPair(img1, img2):
-    # Detector
     detector = cv2.ORB_create()
-    # Imagen 1
     imgGray1 = cv2.cvtColor(img1,cv2.COLOR_BGR2GRAY)
     ret1, mask1 = cv2.threshold(imgGray1, 1, 255, cv2.THRESH_BINARY)
     # find key points
     kp1, des1 = detector.detectAndCompute(imgGray1,None)
-    
-    # Imagen 2
+
     imgGray2 = cv2.cvtColor(img2,cv2.COLOR_BGR2GRAY)
     ret2, mask2 = cv2.threshold(imgGray2, 1, 255, cv2.THRESH_BINARY)
     # find key points
@@ -47,9 +44,9 @@ def stitchPair(img1, img2):
     A = cv2.estimateRigidTransform(src_pts,dst_pts,fullAffine=False)
 
     if (A is None):
-    print("A is None")
-    HomogResult = cv2.findHomography(src_pts,dst_pts,method=cv2.RANSAC)
-    H = HomogResult[0]
+      print("A is None")
+      HomogResult = cv2.findHomography(src_pts,dst_pts,method=cv2.RANSAC)
+      H = HomogResult[0]
 
     h1,w1 = img1.shape[:2]
     h2,w2 = img2.shape[:2]
@@ -76,7 +73,7 @@ def stitchPair(img1, img2):
     translation = np.float32(([1,0,-1*xMin],[0,1,-1*yMin],[0,0,1]))
 
     warpedResImg = cv2.warpPerspective(img1, translation, (xMax-xMin, yMax-yMin))
-
+    
     if (A is None):
         fullTransformation = np.dot(translation, H)
         warpedImage2 = cv2.warpPerspective(img2, fullTransformation, (xMax-xMin, yMax-yMin))
@@ -89,7 +86,7 @@ def stitchPair(img1, img2):
     pass
 
 if __name__ == "__main__":
-    img1 = cv2.imread('DJI_0874.JPG')
-    img2 = cv2.imread('DJI_0875.JPG')
-    stitchPair(img1, img2)
-    pass
+  img1 = cv2.imread('DJI_0398.JPG')
+  img2 = cv2.imread('DJI_0397.JPG')
+  stitchPair(img1, img2)
+  pass

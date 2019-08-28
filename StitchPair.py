6ea5c67fabbef84de705379dd624da0b4ad8dc77
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-08-28 16:56:46 -0500

    Ok

--- StitchPair.py
+++ StitchPair.py
@@ -103,8 +103,11 @@ def Combinar(image_dir, key_frame, output_dir):
     print ("dir_lis: ", dir_list)
     print ("Type: ", type(dir_list))
     for image in dir_list[:1]:
-        result = cv2.imread(image)
+        # result = cv2.imread(image)
         print ("image: ",image)
+    
+    result = cv2.imread(dir_list[:1])
+
     for image in dir_list[1:]:
         # result = stitchPair(result, cv2.imread(image))
         result = cv2.imread(image)

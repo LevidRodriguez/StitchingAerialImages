f9249445fc39c055557fad0771c325eb61c272d0
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-08-28 13:51:08 -0500

    Ok

--- StitchPair.py
+++ StitchPair.py
@@ -102,12 +102,9 @@ def Combinar(image_dir, key_frame, output_dir):
     # dir_list = filter(lambda x: x != key_frame, dir_list)
     print ("dir_lis: ", dir_list)
     print ("Type: ", type(dir_list))
-    # result = cv2.imread(dir_list[:1])
+    result = cv2.imread(dir_list[:1])
     print(dir_list[:1])
-    for file in dir_list[1:]:
-        print (file)
-
-    for image in dir_list:
+    for image in dir_list[1:]:
         # result = stitchPair(result, cv2.imread(image))
         # print(dir_list[im age])
         print(image)

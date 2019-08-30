67ae7ecdc550c0ae1e3d75dbc4aa69699005012a
Levid Rodriguez <levid.rodriguez@gmail.com>
2019-08-30 13:53:12 -0500

    OK

--- StitchPair.py
+++ StitchPair.py
@@ -116,18 +116,18 @@ def Combinar(image_dir, key_opt):
     for image in dir_list[1:]:
         print("Processing ",image)
         result = stitchPair(result, en.compress(cv2.imread(image)), key_opt)
-        # result = stitchPair(result, cv2.imread(image), key_opt)
-        # h, w = result.shape[:2]
-        # if h > 4000 and w > 4000:
-        #     if h > 4000:
-        #         h = h * (4000/h)
-        #         w = w * (4000/h)
-        #     elif w > 4000:
-        #         h = h * (4000/w)
-        #         w = w * (4000/w)
-        #     result = cv2.resize(result, (int(w),int(h)))
-        #     cv2.imwrite("result.png", result)
-        #     pass
+        result = stitchPair(result, cv2.imread(image), key_opt)
+        h, w = result.shape[:2]
+        if h > 4000 and w > 4000:
+            if h > 4000:
+                h = h * (4000/h)
+                w = w * (4000/h)
+            elif w > 4000:
+                h = h * (4000/w)
+                w = w * (4000/w)
+            result = cv2.resize(result, (int(w),int(h)))
+            cv2.imwrite("result.png", result)
+            pass
         cv2.imwrite("result.png", result)
         pass
     pass
